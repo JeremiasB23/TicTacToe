5aac1c2d61aa7276f2fe8438b2dca71565ac52ad
JeremiasB23 <jerebutassi@gmail.com>
2022-01-25 10:40:52 -0300

    I Started the project from zero

---
+++ script.js
@@ -0,0 +1,40 @@
+const boardArray = [];
+let turns = 0;
+
+function createSquare() {
+  const board = document.querySelector(".board");
+
+  let square = new Array();
+
+  for (let i = 0; i < 9; i++) {
+    //*Create all the squares and asing them as a childs of board
+    square[i] = document.createElement("div");
+    board.appendChild(square[i]);
+  }
+
+  for (let i = 3; i < 9; i++) {
+    //*Reduce the top margin since the 4 square
+    square[i].style.marginTop = "-5px";
+  }
+
+  for (elem of square) {
+    elem.addEventListener("click", function () {
+      if (turns % 2 == 0) {
+        this.style.backgroundColor = "red";
+        boardArray.splice(elem, 0, "O"); //!I have to check how to add the 0 to the specific square in the array
+      } else {
+        this.style.backgroundColor = "blue";
+        boardArray.splice(elem, 0, "X");
+      }
+
+      turns++;
+      console.log(boardArray);
+    });
+  }
+}
+
+function logicBoard() {
+  createSquare();
+}
+
+logicBoard();
