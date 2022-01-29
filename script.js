9539c358a26c4190b221c87bacacc2a09c8a053e
JeremiasB23 <jerebutassi@gmail.com>
2022-01-29 12:14:53 -0300

    All the logic is almost finished, but i have to fix some aspects

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const boardArray = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
+const boardArray = ["", "", "", "", "", "", "", "", ""];
 let turns = 0;
 
 function createSquare() {
   const board = document.querySelector(".board");
 
   let square = new Array();
+
+  circulito = document.createElement("div");
+  circulito2 = document.createElement("div");
+
+  cruz = document.createElement("div");
+  cruz2 = document.createElement("div");
+
+  circulito.className = "circulito";
+  circulito2.className = "circulito2";
+
+  cruz.className = "cruz";
+  cruz2.className = "cruz2";
 
   for (let i = 0; i < 9; i++) {
     //*Create all the squares and asing them as a childs of board
@@ -17,24 +29,62 @@ function createSquare() {
     square[i].style.marginTop = "-5px";
   }
 
-  for (elem of square) {
-    elem.addEventListener("click", function () {
-      if (turns % 2 == 0) {
-        this.style.backgroundColor = "red";
-        boardArray.splice(elem.value, elem.value, "O"); //!I have to check how to add the 0 to the specific square in the array
-      } else {
-        this.style.backgroundColor = "blue";
-        boardArray.splice(elem.value, elem.value, "X");
+  for (let i = 0; i < 9; i++) {
+    square[i].addEventListener("click", function () {
+      if (boardArray[i] != "O" && boardArray[i] != "X") {
+        if (turns % 2 == 0) {
+          //this.style.backgroundColor = "red";
+          //square[i].appendChild(circulito);
+          //circulito.appendChild(circulito2);
+          this.style.backgroundColor = "red";
+          boardArray.splice(i, 1, "O");
+          checkWinner();
+          turns++;
+        } else {
+          this.style.backgroundColor = "blue";
+          // this.appendChild(cruz);
+          // cruz.appendChild(cruz2);
+          boardArray.splice(i, 1, "X");
+          checkWinner();
+          turns++;
+        }
       }
-
-      turns++;
-      console.log(boardArray);
     });
   }
 }
 
+function checkWinner() {
+  if (
+    (boardArray[0] == "O" && boardArray[1] == "O" && boardArray[2] == "O") ||
+    (boardArray[2] == "O" && boardArray[5] == "O" && boardArray[8] == "O") ||
+    (boardArray[8] == "O" && boardArray[7] == "O" && boardArray[6] == "O") ||
+    (boardArray[6] == "O" && boardArray[3] == "O" && boardArray[0] == "O") ||
+    (boardArray[1] == "O" && boardArray[4] == "O" && boardArray[7] == "O") ||
+    (boardArray[3] == "O" && boardArray[4] == "O" && boardArray[5] == "O") ||
+    (boardArray[0] == "O" && boardArray[4] == "O" && boardArray[8] == "O") ||
+    (boardArray[2] == "O" && boardArray[4] == "O" && boardArray[6] == "O")
+  ) {
+    console.log("O GANASTE");
+  } else if (
+    (boardArray[0] == "X" && boardArray[1] == "X" && boardArray[2] == "X") ||
+    (boardArray[2] == "X" && boardArray[5] == "X" && boardArray[8] == "X") ||
+    (boardArray[8] == "X" && boardArray[7] == "X" && boardArray[6] == "X") ||
+    (boardArray[6] == "X" && boardArray[3] == "X" && boardArray[0] == "X") ||
+    (boardArray[1] == "X" && boardArray[4] == "X" && boardArray[7] == "X") ||
+    (boardArray[3] == "X" && boardArray[4] == "X" && boardArray[5] == "X") ||
+    (boardArray[0] == "X" && boardArray[4] == "X" && boardArray[8] == "X") ||
+    (boardArray[2] == "X" && boardArray[4] == "X" && boardArray[6] == "X")
+  ) {
+    console.log("X GANASTE");
+  }
+
+  //*EL if para finalizar que sea si todas las casillas del array
+  //*estan llenas
+}
+
 function logicBoard() {
   createSquare();
+  checkWinner();
 }
 
 logicBoard();
